2d76802c5b63d8ed562532980ab2a6a5efb4ff3d
Ivan Dudin <van.dud.niga@yandex.ru>
2017-10-10 00:32:06 +0300

    eshe odin commit na translite

---
+++ Android_clicker/app/src/main/java/ru/vandud/android_clicker/MainView.java
@@ -0,0 +1,16 @@
+package ru.vandud.android_clicker;
+
+import android.content.Context;
+import android.view.View;
+
+/**
+ * Created by Vandud on 10.10.2017.
+ */
+
+
+
+public class MainView extends View {
+    public MainView(Context context) {
+        super(context);
+    }
+}
